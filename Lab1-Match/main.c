eb81500ef60a4c2f12230944e60f42de1ca40c44
paarthlakhani <paarthlakhani@localhost.localdomain>
2016-08-28 18:33:37 -0400

    Deleted the test file and added the main file

---
+++ Lab1-Match/main.c
@@ -0,0 +1,11 @@
+/*
+    Author: Paarth Lakhani
+    uid: u0936913
+*/
+#include<stdio.h>
+
+int main()
+{
+    printf("Started the Assignment.!");
+    return 0;
+}
--- Lab1-Match/test.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-
-int main()
-{
-  printf("Hello World.!/nGreat. Finally centOS");
-  return 0;
-}
