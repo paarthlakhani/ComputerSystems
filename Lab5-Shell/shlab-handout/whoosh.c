367fe325f133609aaa5a03c5cc01aa465835381d
lakhani <lakhani@lab1-25.eng.utah.edu>
2016-11-11 16:33:56 -0700

    First checkpoint 90% done

--- Lab5-Shell/shlab-handout/whoosh.c
+++ Lab5-Shell/shlab-handout/whoosh.c
@@ -16,7 +16,6 @@ static void run_script(script *scr);
 static void run_group(script_group *group);
 static void run_command(script_command *command);
 static void set_var(script_var *var, int new_value);
-static void exclusive_command(script_command *command);
 static void close_pipes(file_descriptor* d, int num_commands);
 
 /* You probably shouldn't change main at all. */
@@ -34,47 +33,65 @@ int main(int argc, char **argv) {
   return 0;
 }
 
-static void run_script(script *scr) {
-  if (scr->num_groups == 1)
- {
-    run_group(&scr->groups[0]);
-  } else 
-    {
-      printf("The number of groups are: %d",scr->num_groups);
-    /* You'll have to make run_script do better than this */
-    //fail("only 1 group supported");
-  }
-}
-
-/*
-static void close_pipes(  , file_descriptor d[])
+static void run_script(script *scr) 
 {
-  
+  int i;
+
+  for( i = 0; i < scr->num_groups; i++)
+    {
+       int j;
+       for( j= 0; j< scr->groups[i].repeats; j++ )
+	run_group(&scr->groups[i]);
+    }
 }
-*/ 
 
 static void run_group(script_group *group) 
 {
-  /* You'll have to make run_group do better than this, too */
-  if (group->repeats != 1)
-    fail("only repeat 1 supported");
+    script_var* variable = NULL;
   if (group->result_to != NULL)
-    fail("setting variables not supported");
+    {
+        variable = group->result_to;
+    }
+
 
   if (group->num_commands == 1) 
-{
-    run_command(&group->commands[0]);
-  } 
+    {
+
+      pid_t pid = Fork();
+      if(pid == 0)
+	{ 
+	  Setpgid(0,0);
+	  run_command(&group->commands[0]);
+	}
+      else
+	{
+	  int child_status;
+	  Waitpid(pid,&child_status,0);
+
+	  int result = 0;
+	      
+	  if(WIFEXITED(child_status))
+	    {
+	      result = WEXITSTATUS(child_status);
+	    }
+	  else if(WIFSIGNALED(child_status))
+	    {
+	      result = WTERMSIG(child_status);
+	      result=result*-1;
+	    }    
+	      
+	  if(variable!=NULL)
+	    {
+	      set_var( variable , result);
+	    }
+	}
+    } 
   else 
     {
-      if(group->mode == 0)
+      int num_commands = group->num_commands;
+      if(group->mode == 1) // And group
 	{
-	 printf("I am in group_single");
-	}
-      else if(group->mode == 1)
-	{
-	  int num_commands = group->num_commands;
-	  //func_descriptor d[num_commands - 1];
+	  //	  int num_commands = group->num_commands;
 	  file_descriptor* d;
 	  d = malloc(sizeof(int *) * (2*(num_commands-1)));
 	   int i;
@@ -85,59 +102,84 @@ static void run_group(script_group *group)
 
 	  for( i = 0; i < num_commands; i++)
 	    {
-	      if(Fork() == 0)
+	      pid_t pid = Fork();
+	      script_command *command = &group->commands[i];
+
+	      if(pid == 0)
 		{		  
-		  script_command *command;
 		  if(i == 0 )
 		    {
-		      command = &group->commands[0];
 		      Dup2(d[0].fds[1],1);
 		    }
 		  else if( i == num_commands -1 )
 		    {
-		      command = &group->commands[num_commands - 1];
 		      Dup2(d[num_commands - 2].fds[0],0); // last file descriptor
 		    }
 		  else
 		    {
-		      //Dup2(current_input_output,desired_input_output);
+		      Dup2(d[i-1].fds[0],0);
+		      Dup2(d[i].fds[1],1);
 		    }
+
 		  close_pipes(d,num_commands);
-		   run_command(command);
+		  Setpgid(0,0);
+		  run_command(command);
+		} // end of fork
+	      else
+		{
+		  if(command->pid_to!=NULL)
+		    {
+		      set_var( command->pid_to , pid );
+		    }
 		}
-	    }
-	  
+	    } // for-loop
 	  
 	  close_pipes(d,num_commands);
 	  for( i = 0 ; i < num_commands; i++)
 	    {
 	      int child_status;
-	      pid_t pid = Waitpid(-1,&child_status,0);
+	      Waitpid(-1,&child_status,0);
+	      int result = 0;
+	      
+	      if(WIFEXITED(child_status))
+		{
+		  result = WEXITSTATUS(child_status);
+		}
+	      else if(WIFSIGNALED(child_status))
+		{
+		  result = WTERMSIG(child_status);
+		}
+	      
+	      if(variable!=NULL)
+		{
+		  set_var( variable , result);
+		}
 	    }
-	  
-	  free(d);
-	  /*
-	  script_command *command1 = &group->commands[0];
-	  int first_fd[2];
-	  Pipe(first_fd);
-	  exclusive_command(command1,first_fd[1],1); // First command
-	  
-	  int i;
-	    for(i = 1 ; i < group->num_commands-1;i++)
-	    {
-	      // Use Pipe and Dup2
-	      //run_command(&group->commands[i]);
-	      }
 
-	  script_command* command_last = &group->commands[group->num_commands-1];
-	  exclusive_command(command_last, first_fd[0],0); // Last command
-	  */
-	}
-      else
+	  free(d);
+	} // End AND group
+      else // OR group
 	{
-	   printf("I am in group_or");
+	  int i;
+	  pid_t pids[num_commands];
+	  
+	  for( i = 0 ; i < num_commands; i++)
+	    {
+	      pid_t current_pid = Fork();
+	      script_command *command = &group->commands[i];	      
+	      
+	      if(current_pid == 0)
+		{
+		  Setpgid(0,0);
+		  run_command(command);	  
+		}
+	      else
+		{
+		  pids[i] = current_pid;
+		}
+	    } // end of the for loop
+	  
 	}
-      int i = 0;
     }
 }
 
@@ -151,44 +193,10 @@ static void close_pipes(file_descriptor* d, int num_commands)
     }
 }
 
-static void exclusive_command(script_command *command)
-{
-  const char **argv;
-  int j;
-  argv = malloc(sizeof(char *) * (command->num_arguments + 2));
-  argv[0] = command->program;
-  
-  for (j = 0; j < command->num_arguments; j++) 
-    {
-      if (command->arguments[j].kind == ARGUMENT_LITERAL)
-	argv[j+1] = command->arguments[j].u.literal;
-      else
-	{
-	  argv[j+1] = command->arguments[j].u.var->value;
-	}
-    }
-
-  argv[command->num_arguments + 1] = NULL;
-  Execve(argv[0], (char * const *)argv, environ);
-
-}
-
 static void run_command(script_command *command) 
 {
-  const char **argv;
+  const char **argv;  
   int i;
-
-  /*
-  if(command->pid_to!=NULL)
-    {
-      printf("%s",command->pid_to->name);
-      printf("%s",command->pid_to->value);
-    }
-  else
-    {
-      printf("There's no variable there.\n");
-    }
-  */
 
   argv = malloc(sizeof(char *) * (command->num_arguments + 2));
   argv[0] = command->program;
@@ -205,76 +213,9 @@ static void run_command(script_command *command)
   
   argv[command->num_arguments + 1] = NULL;
   Execve(argv[0], (char * const *)argv, environ);
-  
-  /*
-  printf("Arguments are: %d\n", (command->num_arguments));
-  for (i = 0; i <(command->num_arguments); i++) 
-    {
-      printf("%s\n",argv[i]);
-    }
-  */
-  // You have to create a shell pipeline.
-  /* 
-    pid_t process_id = Fork();
-  
-    if(process_id == 0) // child process.
-      {
-	Execve(argv[0], (char * const *)argv, environ);
-      }
-    else
-      {
-	int child_status;
-	Waitpid(process_id,&child_status,0);
-      }
+
    free(argv);
-  */
 }
-
-
-/* This run_command function is a good start, but note that it runs
-   the command as a replacement for the `whoosh` script, instead of
-   creating a new process. */
-/*
-static void run_command(script_command *command)
-{
-  // I'll be having two pipes.
-  // /bin/echo -n rumor | /usr/bin/wc -c
-
-  int fdsA[2]; // process 1.
-  //  int fdsB[2]; // process 2.
-  
-  pid_t pid;
-
-  Pipe(fdsA);
-  pid = Fork();
-  int argument_count = 2;
-
-  if( pid == 0)
-    {
-      const char **argv;
-      argv = malloc(sizeof(char *) * (argument_count+2));
-      argv[0] = "/bin/echo";
-      argv[1] = "-n";
-      argv[2] = "rumor";
-      argv[3] =  NULL;
-      Dup2(fdsA[1],1);
-      Close(fdsA[1]);
-      Execve("/bin/echo", (char * const *)argv, environ);
-    }
-  else
-    {
-      int arg_count = 3;
-      const char **argv_other;
-      argv_other = malloc(sizeof(char *) * (arg_count));
-      argv_other[0] = "/usr/bin/wc";
-      argv_other[1] = "-c";
-      argv_other[2] = NULL;
-      Dup2(fdsA[0],0);
-      Close(fdsA[1]);
-      Execve("/usr/bin/wc", (char * const *)argv_other, environ);
-    }
-}
-*/
 
 /* You'll likely want to use this set_var function for converting a
    numeric value to a string and installing it as a variable's
@@ -283,6 +224,6 @@ static void run_command(script_command *command)
 static void set_var(script_var *var, int new_value) {
   char buffer[32];
   free((void *)var->value);
-  snprintf(buffer, sizeof(buffer), "%d", new_value);
+   snprintf(buffer, sizeof(buffer), "%d", new_value);
   var->value = strdup(buffer);
 }
