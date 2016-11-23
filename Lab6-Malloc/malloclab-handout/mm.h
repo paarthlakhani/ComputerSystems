bff81dd3ebdcba22cfacca2683ccc704a639ca8f
lakhani <lakhani@lab1-1.eng.utah.edu>
2016-11-22 22:45:04 -0700

    Modified the header file

--- Lab6-Malloc/malloclab-handout/mm.h
+++ Lab6-Malloc/malloclab-handout/mm.h
@@ -3,3 +3,6 @@
 extern int mm_init (void);
 extern void *mm_malloc (size_t size);
 extern void mm_free (void *ptr);
+extern void *extend(size_t size, void *current_block);
+extern void set_allocated(void *bp, size_t size);
+extern void *coalesce(void *bp);
